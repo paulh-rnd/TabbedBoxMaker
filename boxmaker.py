fd95238037527412751c2b7f11698c45bb853ce3
John Slee <john.slee@fairfaxmedia.com.au>
2016-08-11 18:08:29 +1000

    Schroff: missing os import

--- boxmaker.py
+++ boxmaker.py
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 __version__ = "0.86" ### please report bugs, suggestions etc to bugs@twot.eu ###
 
-import sys,inkex,simplestyle,gettext,math
+import os,sys,inkex,simplestyle,gettext,math
 _ = gettext.gettext
 
 def log(text):
